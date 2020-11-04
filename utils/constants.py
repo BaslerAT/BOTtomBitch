4e429c90d18bfc8cb2ad39f92eb576ec99bed50b
Me <me@memail.com>
2020-11-03 23:48:39 -0500

    initial commit

---
+++ utils/constants.py
@@ -0,0 +1,12 @@
+class MESSAGE:
+    MAX_LEN = 2000
+    CODE_BLOCK = "```"
+
+
+class EMOJIS:
+    CMD_CONFIRM = "\N{WHITE HEAVY CHECK MARK}"
+    CMD_FAIL = "\N{CROSS MARK}"
+
+
+class DIR:
+    FFMPEG = "V:\\Programs\\ffmpeg-2020-10-31-git-3da35b7cc7-essentials_build\\bin\\ffmpeg.exe"
